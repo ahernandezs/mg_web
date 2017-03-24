827a5d0ae37fa0b43a813185aa5d25e750c72a50
ahernandezs <ahernandezs@anzen.com.mx>
2017-03-24 11:33:24 -0600

    report complete

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -16,4 +16,17 @@ export class HttpClient {
     return this.http.post(url, data, options);
   }
   
+  getSesionToken(): string{
+      var x = document.cookie.split(';');
+      var toquen;
+      for(var i=0; i < x.length; i++) {
+          var c = x[i];
+          while (c.charAt(0)==' ') c = c.substring(1,c.length);
+          if (c.indexOf('X-AUTH-TOKEN=') == 0)
+            toquen = c.substring('X-AUTH-TOKEN='.length,c.length);
+      }
+      return toquen;
+  }
+
+
 }
