610f95c902879a872d847a67f5d4d23125ca37b8
Máximo Hdz <ahernandezs@anzen.com.mx>
2017-05-04 13:34:53 -0500

    pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,12 +22,13 @@ import { AuthService } from './services/auth.services';
 import { HttpClient } from './services/http.service';
 
 import { AuthGuard } from './utils/auth.guards';
-import { YesorNo } from './utils/pipes';
+import { YesorNo, DateRange } from './utils/pipes';
 import { Utils } from './utils/utils';
 
 @NgModule({
   declarations: [
     YesorNo,
+    DateRange,
     AppComponent,
     LoginComponent,
     AccessComponent,
