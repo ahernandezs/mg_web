8bebecf7a8f70a8bbb9b8c37f1173fb15409a1b4
Máximo Hdz <ahernandezs@anzen.com.mx>
2017-05-04 13:41:53 -0500

    :thinking:

--- src/app/utils/pipes.ts
+++ src/app/utils/pipes.ts
@@ -10,6 +10,7 @@ export class YesorNo implements PipeTransform {
 @Pipe({name: 'dateRange'})
 export class DateRange implements PipeTransform {
   transform(response: string): string {
+    console.log('en el filtro: '+response);
     let tmp = response.split(' ');
     return tmp[0] === tmp[2] ? tmp[0] : tmp[0] + ' hasta ' + tmp[2];
   }
